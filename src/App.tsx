764b78541591f677300ad02e3c9ebe72bca55934
David Kormushoff <kormie@gmail.com>
2018-08-04 14:39:56 -0700

    Add some UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,54 @@
+import { AppBar, IconButton, Toolbar, WithStyles } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import { createStyles, withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
 import * as React from 'react';
-import './App.css';
 
 import logo from './logo.svg';
 
-class App extends React.Component {
+const styles = (theme: {}) =>
+  createStyles({
+    flex: {
+      flexGrow: 1,
+    },
+    header: {
+      backgroundColor: 'red',
+      textAlign: 'center',
+    },
+  });
+
+interface IProps extends WithStyles<typeof styles> {}
+
+class App extends React.Component<IProps> {
   public render() {
+    const { classes } = this.props;
     return (
-      <div className="App">
-        <header className="App-header">
+      <Grid container={true} spacing={24}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="title"
+              color="inherit"
+              className={classes.flex}
+            >
+              Stats
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Grid item={true} className="App-header" md={6}>
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
+        </Grid>
+        <Grid item={true} className="App-intro" md={6}>
           To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+        </Grid>
+      </Grid>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
